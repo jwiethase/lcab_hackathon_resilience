dd48310ad4c4225578f2941a3a2731e7e529bbd9
HelenMylne <hkm513@york.ac.uk>
2022-05-18 11:56:58 +0100

    grouped biotime data to identify sites with many years of recording

--- R/biotime_data_exploration.R
+++ R/biotime_data_exploration.R
@@ -10,3 +10,13 @@ summary(bt_f)
 
 # every species for every plot - binary count for each year
 
+bt_f_summary <- bt_f %>%
+  group_by(plot, year, genus) %>%
+  tally()
+
+bt_f_summary <- bt_f_summary%>%
+  group_by(plot, genus) %>%
+  tally()
+
+bt_f_s_50 <- bt_f_summary %>%
+  filter(n >= 50)
