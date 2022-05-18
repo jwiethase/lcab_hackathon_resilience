a4128297f4c80802e3b18b3b168b22853db326ae
HelenMylne <hkm513@york.ac.uk>
2022-05-18 15:30:13 +0100

    continuing data exploration at different scales

--- R/biotime_data_exploration.R
+++ R/biotime_data_exploration.R
@@ -1,19 +1,14 @@
 ### data exploration of full data
-
-
 library(tidyverse)
 
-bt_f <- read_csv("Data/biotime_full.csv")
-bt_f <- janitor::clean_names(bt_f)
+bt_f <- read_csv("Data/biotime_full.csv") %>% janitor::clean_names()
+bt_meta <- read_csv("Data/biotime_metadata.csv") %>% janitor::clean_names()
 
-bt_meta <- read_csv("Data/biotime_metadata.csv")
-bt_meta <- janitor::clean_names(bt_meta)
-
+####### old stuff ########
 summary(bt_f)
 
 # every species for every plot - binary count for each year
 bt_s <- bt_f %>% select(c('year','plot','genus')) %>% distinct()
-
 
 length(unique(bt_s$genus))
 
@@ -38,6 +33,219 @@ bt_f_s_50 <- bt_f_summary %>%
 bt_f_s_50 %>%
   group_by(plot) %>%
   tally()
+
+bt_f$latitude <- round(bt_f$latitude, 2)
+bt_f$longitude <- round(bt_f$longitude, 2)
+bt_f$location <- paste(bt_f$latitude, bt_f$longitude, sep = '_')
+
+bt_f_summary <- bt_f %>%
+  group_by(location, year, genus) %>%
+  tally()
+
+bt_f_summary <- bt_f_summary %>%
+  group_by(location, genus) %>%
+  tally()
+
+bt_f_s_50 <- bt_f_summary %>%
+  filter(n >= 50)
+
+bt_f_s_50 %>%
+  group_by(plot) %>%
+  tally()
+
+#plot(bt_f$longitude ~ bt_f$latitude, pch = 16, cex = 0.2, col = rgb(0,0,1,0.2)) # map sightings, but takes a huge amount of memory and time
+
+bt_ordered$gen_yr <- paste(bt_ordered$genus, bt_ordered$year, '_')
+bt_ordered <- bt_ordered[order(bt_ordered$gen_yr),]
+bt_o_s <- bt_ordered %>% select(c('year','genus','location'))
+
+counts <- aggregate(rep(1, NROW(bt_o_s)), by = list(genus = bt_o_s$genus, location = bt_o_s$location), sum)
+counts
+
+continuous <- function(years){
+  years <- years[!is.na(years)]
+  start_year <- min(years)
+  end_year <- max(years)
+  range_year <- range(years)
+  max_gap <- max(years[-1] - years[-nrow(years)])
+  
+  sightings
+}
+
+  
+  
+
+year_diff <- aggregate(bt_o_s$year, by = list(genus = bt_o_s$genus, location = bt_o_s$location), range)
+year_diff
+
+
+
+bt_o_s$year_count <- NA
+bt_o_s$year_gap <- NA
+
+for(i in 2:10){
+  genus <- bt_o_s[bt_o_s$genus == bt_o_s$genus[i] & bt_o_s$location == bt_o_s$location[i],]
+  genus <- genus[!is.na(genus$genus),]
+  bt_o_s$year_count[i] <- nrow(genus)
+  
+  if(bt_o_s$genus[i] == bt_o_s$genus[i-1] & bt_o_s$location[i] == bt_o_s$location[i-1]){
+    bt_o_s$year_gap[i] <- bt_o_s$year[i] - bt_o_s$year[i-1]
+  }
+  else(bt_o_s$year_gap[i] <- 9999)  
+}
+
+####### start over #########
+terr_animals <- bt_meta[bt_meta$realm == 'Terrestrial' & bt_meta$taxa != 'Fungi' & bt_meta$taxa != 'Terrestrial plants',]
+
+bt_ta <- bt_f[bt_f$study_id %in% terr_animals$study_id,]
+rm(bt_f, bt_meta)
+
+bt_ta$loc <- paste0(round(bt_ta$latitude,2), '_', round(bt_ta$longitude,2))
+bt_ta_s <- bt_ta[,c('study_id','year','genus','loc')]
+
+bt_ta_summary <- bt_ta_s %>%
+  group_by(loc, year, genus) %>%
+  tally()
+
+bt_ta_summary <- bt_ta_summary%>%
+  group_by(loc, genus) %>%
+  tally()
+
+hist(bt_ta_summary$n)
+gen_loc <- bt_ta_summary %>%
+  group_by(loc) %>%
+  tally()
+
+
+bt_ta_s_10 <- bt_ta_summary %>%
+  filter(n >= 10)
+
+gen_loc <- bt_ta_s_10 %>%
+  group_by(loc) %>%
+  tally()
+
+bt_ta_s_20 <- bt_ta_summary %>%
+  filter(n >= 20)
+
+gen_loc <- bt_ta_s_20 %>%
+  group_by(loc) %>%
+  tally()
+
+gen_loc_sep <- separate(gen_loc, loc, into = c('lat','long'), sep = '_', remove = F)
+
+plot(gen_loc_sep$long ~ gen_loc_sep$lat, las = 1, pch = 16, col = rgb(0,0,1,0.2),
+     xlab = 'latitude', ylab = 'longitude')
+abline(lty = 2, v = 0, h = 0)
+text('equator', x = 20, y = 5)
+text('Greenwich', x = -3, y = 50, srt = 90)
+
+library(sf)
+library(rnaturalearth)
+library(rnaturalearthdata)
+
+world <- ne_countries(scale = 'medium', returnclass = 'sf')
+class(world)
+
+#ggplot()+
+#  geom_sf(data = world)+
+#  coord_sf(ylim = c(-40, 80), xlim = c(-150,150))+
+#  geom_point(data = gen_loc_sep, aes(y = lat, x = long))
+
+maps::map('world')
+points(gen_loc_sep$long, gen_loc_sep$lat, cex = 0.5, pch = 16, col = 'blue')
+
+####### birds only #####
+bt_f <- read_csv("Data/biotime_full.csv") %>% janitor::clean_names()
+bt_meta <- read_csv("Data/biotime_metadata.csv") %>% janitor::clean_names()
+
+terr_birds <- bt_meta[bt_meta$realm == 'Terrestrial' & bt_meta$taxa == 'Birds',]
+
+birds <- bt_f[bt_f$study_id %in% terr_birds$study_id,]
+rm(bt_f, bt_meta)
+
+birds$loc <- paste0(round(birds$latitude,2), '_', round(birds$longitude,2))
+
+bt_ta_summary <- birds %>%
+  group_by(loc, year, genus) %>%
+  tally()
+
+bt_ta_summary <- bt_ta_summary%>%
+  group_by(loc, genus) %>%
+  tally()
+
+bt_ta_s_10 <- bt_ta_summary %>% filter(n >= 10)
+gen_loc_10 <- bt_ta_s_10 %>%
+  group_by(loc) %>%
+  tally()
+gen_loc10_sep <- separate(gen_loc_10, loc, into = c('lat','long'), sep = '_', remove = F)
+
+bt_ta_s_20 <- bt_ta_summary %>% filter(n >= 20)
+gen_loc_20 <- bt_ta_s_20 %>%
+  group_by(loc) %>%
+  tally()
+gen_loc20_sep <- separate(gen_loc_20, loc, into = c('lat','long'), sep = '_', remove = F)
+
+library(sf)
+library(rnaturalearth)
+library(rnaturalearthdata)
+
+world <- ne_countries(scale = 'medium', returnclass = 'sf')
+class(world)
+
+maps::map('world')
+points(gen_loc10_sep$long, gen_loc10_sep$lat, cex = 0.5, pch = 16, col = 'blue')
+points(gen_loc20_sep$long, gen_loc20_sep$lat, cex = 0.5, pch = 16, col = 'red')
+
+
+### data exploration of full data
+library(tidyverse)
+
+bt_f <- read_csv("Data/biotime_full.csv") %>% janitor::clean_names()
+bt_meta <- read_csv("Data/biotime_metadata.csv") %>% janitor::clean_names()
+
+####### everything everything ########
+bt_f
+bt_meta
+
+bt_f$loc <- paste0(round(bt_f$latitude,2), '_', round(bt_f$longitude,2))
+bt_f_s <- bt_f[,c('study_id','year','genus','loc')]
+
+bt_f_summary <- bt_f_s %>%
+  group_by(loc, year, genus) %>%
+  tally()
+
+bt_f_summary <- bt_f_summary%>%
+  group_by(loc, genus) %>%
+  tally()
+
+gen_loc <- bt_f_summary %>%
+  group_by(loc) %>%
+  tally()
+gen_loc_sep <- separate(gen_loc, loc, into = c('lat','long'), sep = '_', remove = F)
+
+plot(gen_loc_sep$long ~ gen_loc_sep$lat, las = 1, pch = 16, col = rgb(0,0,1,0.2),
+     xlab = 'latitude', ylab = 'longitude')
+abline(lty = 2, v = 0, h = 0)
+text('equator', x = 20, y = 5)
+text('Greenwich', x = -3, y = 50, srt = 90)
+
+library(sf)
+library(rnaturalearth)
+library(rnaturalearthdata)
+
+world <- ne_countries(scale = 'medium', returnclass = 'sf')
+class(world)
+
+#ggplot()+
+#  geom_sf(data = world)+
+#  coord_sf(ylim = c(-40, 80), xlim = c(-150,150))+
+#  geom_point(data = gen_loc_sep, aes(y = lat, x = long))
+
+maps::map('world')
+points(gen_loc_sep$long, gen_loc_sep$lat, cex = 0.5, pch = 16, col = 'blue')
+
+
+
 
 
 
