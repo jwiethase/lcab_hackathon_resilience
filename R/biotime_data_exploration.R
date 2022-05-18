206087087ffd0a0683f60ed90c312ef053f342f2
Molly Brown <mb2340@york.ac.uk>
2022-05-18 12:05:05 +0100

    sumamrising data by year/genus/plot

--- R/biotime_data_exploration.R
+++ R/biotime_data_exploration.R
@@ -10,3 +10,22 @@ summary(bt_f)
 
 # every species for every plot - binary count for each year
 
+bt_s <- bt_f %>% select(c('year','plot','genus')) %>% distinct()
+
+
+length(unique(bt_s$genus))
+
+# create data frame with sightings per genus per year per plot -- long format creates over 300 billion rows
+# presence <- data.frame(plot = rep(sort(unique(bt_s$plot)), length(unique(bt_s$genus))*length(unique(bt_s$year)) ),
+#                       genus = rep(rep(sort(unique(bt_s$genus)),  length(unique(bt_s$plot))), length(unique(bt_s$year))),
+#                       year = rep(sort(unique(bt_s$year))),
+#                       presence = rep(NA, length(unique(bt_s$year))*length(unique(bt_s$plot))*length(unique(bt_s$genus)) ))
+
+
+
+
+
+
+
+
+
