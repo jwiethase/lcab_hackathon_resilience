f1ca3b3e80dec00c38e73e83179fa86214451740
HelenMylne <hkm513@york.ac.uk>
2022-05-18 12:03:28 +0100

    read in metadata

--- R/biotime_data_exploration.R
+++ R/biotime_data_exploration.R
@@ -6,6 +6,9 @@ library(tidyverse)
 bt_f <- read_csv("Data/biotime_full.csv")
 bt_f <- janitor::clean_names(bt_f)
 
+bt_meta <- read_csv("Data/biotime_metadata.csv")
+bt_meta <- janitor::clean_names(bt_meta)
+
 summary(bt_f)
 
 # every species for every plot - binary count for each year
@@ -20,3 +23,9 @@ bt_f_summary <- bt_f_summary%>%
 
 bt_f_s_50 <- bt_f_summary %>%
   filter(n >= 50)
+
+bt_f_s_50 %>%
+  group_by(plot) %>%
+  tally()
+
+
